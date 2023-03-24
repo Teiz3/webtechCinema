2bc052757cec0c4504922cc757a0855afb45719b
Teiz3 <108479294+Teiz3@users.noreply.github.com>
2023-03-24 13:33:49 +0100

    initial server file

---
+++ main.js
@@ -0,0 +1,6 @@
+#!/usr/bin nodejs
+var http = require('http');
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.end('Hello World.');
+}).listen(8022,'localhost');
