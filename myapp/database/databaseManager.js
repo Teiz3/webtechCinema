b2a7c36dd61da68588fe27ad84ff07ddebce9a5a
Teiz3 <108479294+Teiz3@users.noreply.github.com>
2023-03-29 12:58:46 +0200

    database work + small start navbar

--- myapp/database/databaseManager.js
+++ myapp/database/databaseManager.js
@@ -9,6 +9,19 @@ var exists = fs.existsSync(file);
 var sqlite3 = require("sqlite3").verbose();
 var cinemaDb = new sqlite3.Database(file);
 
+cinemaDb.serialize(function(){
+    if(!exists){
+        cinemaDb.run("CREATE TABLE Movies (movieid INT, title TEXT, desc TEXT)");
+        // cinemaDb.run("CREATE Table RegisteredUsers (userid INT, name TEXT, email TEXT, street TEXT, streetno INT, login TEXT, password TEXT, creditcard INT)");
+    }
+
+    var movieAttributes = cinemaDb.prepare("INSERT INTO Movies VALUES (?, ?, ?)");
+    movieAttributes.run(1, "Interstellar", "This is the interstellar movie");
+    movieAttributes.finalize();
+});
+
+cinemaDb.close();
+
 console.log("database is managed");
 
 module.exports = router;
