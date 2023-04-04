6ba2b6e7647b83a90e39ad101f13433ed3c3d525
Teiz3 <108479294+Teiz3@users.noreply.github.com>
2023-04-04 16:41:14 +0200

    description page

--- myapp/public/javascripts/moviedescription.js
+++ myapp/public/javascripts/moviedescription.js
@@ -1,5 +1,13 @@
-console.log(movie);
+const movie = movies[0];
+
+descriptionPage();
 
 function descriptionPage(){
+    if(movie){
+        makeElNode("h1", document.body, movie.title);
+    }
+}
+
+function notFound(){
 
 }
