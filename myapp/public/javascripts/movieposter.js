458bc4fb4cc7e9e96b3304f2889e2c1062739aeb
Teiz3 <108479294+Teiz3@users.noreply.github.com>
2023-04-04 15:00:38 +0200

    added new data into the database

--- myapp/public/javascripts/movieposter.js
+++ myapp/public/javascripts/movieposter.js
@@ -1,7 +1,7 @@
 // console.log("running");
 // console.log(dbMovies);
 
-const maxMoviesPerPage = 2;
+const maxMoviesPerPage = 10;
 
 for(let i = 0; i < Math.min(dbMovies.length, maxMoviesPerPage); i++){
     makePoster(dbMovies[i]);
@@ -9,7 +9,7 @@ for(let i = 0; i < Math.min(dbMovies.length, maxMoviesPerPage); i++){
 
 function makePoster(movie){
     makeElNode("div", document.body, "", "movie-poster", movie.title);
-    makeElNode("img", getElId(movie.title), "", "movie-poster__image", "", {src: "images/" + movie.image, alt: movie.desc});
+    makeElNode("img", getElId(movie.title), "", "movie-poster__image", "", {src: "images/" + movie.image, alt: "poster of the " + movie.title + " movie"});
     makeElNode("div", getElId(movie.title), movie.title, "movie-poster__overlay");
     makeElNode("a", getElId(movie.title), "link", "", "", {href: "/movie/" + movie.title});
 }
