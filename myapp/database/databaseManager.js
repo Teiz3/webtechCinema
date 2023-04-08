c1485c31ae45388a20718315746c1275f478fd02
Teiz3 <108479294+Teiz3@users.noreply.github.com>
2023-04-08 14:23:48 +0200

    first database rework

--- myapp/database/databaseManager.js
+++ myapp/database/databaseManager.js
@@ -1,38 +1,38 @@
 //routing and express requirements
 var express = require('express');
 var router = express.Router();
-var path = require('path');
+// var path = require('path');
 
-var fs = require("fs");
+// var fs = require("fs");
 
-//getting/creating database
-console.log("dbmanger file path correction");
-var file = path.join(__dirname, 'cinema.db');
-console.log("file after join: " + file);
-var exists = fs.existsSync(file);
+// //getting/creating database
+// console.log("dbmanger file path correction");
+// var file = path.join(__dirname, 'cinema.db');
+// console.log("file after join: " + file);
+// var exists = fs.existsSync(file);
 
-var sqlite3 = require("sqlite3").verbose();
-var cinemaDb = new sqlite3.Database(file);
-console.log("dbmaneger cinemaDb: " + cinemaDb);
+// var sqlite3 = require("sqlite3").verbose();
+// var cinemaDb = new sqlite3.Database(file);
+// console.log("dbmaneger cinemaDb: " + cinemaDb);
 
-//accessing the db
-cinemaDb.serialize(function(){
-    console.log(exists);
-    if(!exists){
-        console.log("dbmanger if(!exists) running");
-        console.log("db setup run");
-        //creating the tables
-        cinemaDb.run("CREATE TABLE Movies (movieid INT UNIQUE, title TEXT NOT NULL UNIQUE, desc TEXT, image TEXT, trailer TEXT, trailerID TEXT, PRIMARY KEY(movieid))");
-        // cinemaDb.run("CREATE TABLE RegisteredUsers (userid INT, name TEXT, email TEXT, street TEXT, streetno INT, login TEXT, password TEXT, creditcard INT)");
+// //accessing the db
+// // cinemaDb.serialize(function(){
+//     console.log("exists: " + exists);
+    // if(!exists){
+    //     console.log("dbmanger if(!exists) running");
+    //     console.log("db setup run");
+    //     //creating the tables
+    //     cinemaDb.run("CREATE TABLE Movies (movieid INT UNIQUE, title TEXT NOT NULL UNIQUE, desc TEXT, image TEXT, trailer TEXT, trailerID TEXT, PRIMARY KEY(movieid))");
+    //     // cinemaDb.run("CREATE TABLE RegisteredUsers (userid INT, name TEXT, email TEXT, street TEXT, streetno INT, login TEXT, password TEXT, creditcard INT)");
         
-        //connecting dbfill file to fill the database with data
+    //     //connecting dbfill file to fill the database with data
         var dbfill = require("./dbfill");
-        dbfill.fillMovies();
-    }
-});
+        dbfill.regenerateDatabase();
+    // }
+// });
 
-console.log("dbmanger dbclose statement");
-cinemaDb.close();
+// console.log("dbmanger dbclose statement");
+// cinemaDb.close();
 
-console.log("dbManager end file");
+// console.log("dbManager end file");
 module.exports = router;
