b47b9e21a2f90ce8f6d5c696484cf292d4d6618c
Teiz3 <108479294+Teiz3@users.noreply.github.com>
2023-04-15 20:31:44 +0200

    longer timeout

--- myapp/public/javascripts/movieposter.js
+++ myapp/public/javascripts/movieposter.js
@@ -103,7 +103,7 @@ for(let i=0; i < 7; i++){
 makeElNode("div", getElClass("schedule-container"), "", "schedule-day");
 
 //Get the schedule of the selected weekday
-setTimeout(getSchedule(0), 1000);
+setTimeout(getSchedule(0), 10000);
 
 //Get schedule according to weekday
 function getSchedule(offset){
