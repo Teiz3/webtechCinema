3ec53d1f1bc7212b6cebb7d70623bf71ccbfbe21
Teiz3 <108479294+Teiz3@users.noreply.github.com>
2023-04-16 22:15:17 +0200

    chorme is kut

--- myapp/public/javascripts/profile.js
+++ myapp/public/javascripts/profile.js
@@ -31,7 +31,7 @@ function getOrdersInfo(){
 //Hardcoded because too little time
 function makeProfilePage(userInfo){ 
     // form for fullname
-    makeElNode('form', getElClass('info-container'), '', 'form__name', '', {action: '../db/changeprofile', method: 'POST'});
+    makeElNode('form', getElClass('info-container'), '', 'form__name', '', {action: './../db/changeprofile', method: 'POST'});
     makeElNode('label', getElClass('form__name'), 'Name:', '', '', {for: 'fname'});
     makeElNode('div', getElClass('form__name'), '', 'input-container', 'input-container__fullname');
     makeElNode('input', getElId('input-container__fullname'), '', '', 'fname', {type: 'text', name: 'fname', placeholder: userInfo.fullname});
@@ -39,7 +39,7 @@ function makeProfilePage(userInfo){
     makeElNode('hr', getElClass('info-container'), '', 'hr');
 
     // form for username
-    makeElNode('form', getElClass('info-container'), '', 'form__uname', '', {action: '../db/changeprofile', method: 'POST'});
+    makeElNode('form', getElClass('info-container'), '', 'form__uname', '', {action: './../db/changeprofile', method: 'POST'});
     makeElNode('label', getElClass('form__uname'), 'Username:', '', '', {for: 'uname'});
     makeElNode('div', getElClass('form__uname'), '', 'input-container', 'input-container__uname');
     makeElNode('input', getElId('input-container__uname'), '', '', 'uname', {type: 'text', name: 'uname', placeholder: userInfo.username});
@@ -47,7 +47,7 @@ function makeProfilePage(userInfo){
     makeElNode('hr', getElClass('info-container'), '', 'hr');
 
     // form for email
-    makeElNode('form', getElClass('info-container'), '', 'form__email', '', {action: '../db/changeprofile', method: 'POST'});
+    makeElNode('form', getElClass('info-container'), '', 'form__email', '', {action: './../db/changeprofile', method: 'POST'});
     makeElNode('label', getElClass('form__email'), 'Email:', '', '', {for: 'email'});
     makeElNode('div', getElClass('form__email'), '', 'input-container', 'input-container__email');
     makeElNode('input', getElId('input-container__email'), '', '', 'email', {type: 'text', name: 'email', placeholder: userInfo.email});
@@ -55,7 +55,7 @@ function makeProfilePage(userInfo){
     makeElNode('hr', getElClass('info-container'), '', 'hr');
 
     //Form for password
-    makeElNode('form', getElClass('info-container'), '', 'form__password', '', {action: '../db/changeprofile', method: 'POST'});
+    makeElNode('form', getElClass('info-container'), '', 'form__password', '', {action: './../db/changeprofile', method: 'POST'});
     makeElNode('label', getElClass('form__password'), 'Password:', '', '', {for: 'password'});
     makeElNode('div', getElClass('form__password'), '', 'input-container', 'input-container__password');
     makeElNode('input', getElId('input-container__password'), '', '', 'password', {type: 'password', name: 'password', placeholder: '******'});
@@ -63,7 +63,7 @@ function makeProfilePage(userInfo){
     makeElNode('hr', getElClass('info-container'), '', 'hr');
 
     // form for creditcard
-    makeElNode('form', getElClass('info-container'), '', 'form__creditcard', '', {action: '../db/changeprofile', method: 'POST'});
+    makeElNode('form', getElClass('info-container'), '', 'form__creditcard', '', {action: './../db/changeprofile', method: 'POST'});
     makeElNode('label', getElClass('form__creditcard'), 'Creditcard:', '', '', {for: 'creditcard'});
     makeElNode('div', getElClass('form__creditcard'), '', 'input-container', 'input-container__creditcard');
     makeElNode('input', getElId('input-container__creditcard'), '', '', 'creditcard', {type: 'number', name: 'creditcard', placeholder: '******'});
@@ -71,7 +71,7 @@ function makeProfilePage(userInfo){
     makeElNode('hr', getElClass('info-container'), '', 'hr');
 
     // Form for street name
-    makeElNode('form', getElClass('info-container'), '', 'form__street', '', {action: '../db/changeprofile', method: 'POST'});
+    makeElNode('form', getElClass('info-container'), '', 'form__street', '', {action: './../db/changeprofile', method: 'POST'});
     makeElNode('label', getElClass('form__street'), 'Street:', '', '', {for: 'street'});
     makeElNode('div', getElClass('form__street'), '', 'input-container', 'input-container__street');
     makeElNode('input', getElId('input-container__street'), '', '', 'street', {type: 'text', name: 'street', placeholder: userInfo.street});
@@ -79,7 +79,7 @@ function makeProfilePage(userInfo){
     makeElNode('hr', getElClass('info-container'), '', 'hr');
 
     // Form for street number
-    makeElNode('form', getElClass('info-container'), '', 'form__streetno', '', {action: '../db/changeprofile', method: 'POST'});
+    makeElNode('form', getElClass('info-container'), '', 'form__streetno', '', {action: './../db/changeprofile', method: 'POST'});
     makeElNode('label', getElClass('form__streetno'), 'Street Number:', '', '', {for: 'streetno'});
     makeElNode('div', getElClass('form__streetno'), '', 'input-container', 'input-container__streetno');
     makeElNode('input', getElId('input-container__streetno'), '', '', 'streetno', {type: 'number', name: 'streetno', placeholder: userInfo.streetno, min: '1'});
